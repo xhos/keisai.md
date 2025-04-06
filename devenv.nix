b09e7cd80febf388e4cd52ca164e15e564d59649
xhos <60789741+xhos@users.noreply.github.com>
2025-04-05 21:33:19 -0400

    fix: update build output name and add bin/ to .gitignore

--- devenv.nix
+++ devenv.nix
@@ -2,7 +2,7 @@
   languages.go.enable = true;
 
   scripts = {
-    build.exec = "go build -o bin/api ./cmd";
+    build.exec = "go build -o bin/keisai ./cmd";
     run.exec = "go run ./cmd/main.go";
     start.exec = "go build -o bin ./cmd && ./bin";
     test-all.exec = "go test -v ./...";
